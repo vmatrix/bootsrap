ba8ea5215efa4e43bc1da266df9f122edd989924
vmatrix <vmatrixcloud@gmail.com>
2016-07-15 15:26:21 +0800

    d

--- ubuntu_bash.sh
+++ ubuntu_bash.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 #
-# Ubuntu Equip 
+# Ubuntu Equip for docker
 #  Equip basec/core libs
 # Licence: MIT
 
-sudo apt-get update -y
+apt-get update -y
 
-sudo apt-get install build-essential rsync telnet screen man wget -y
-# sudo apt-get install strace tcpdump -y
-sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
-sudo apt-get install software-properties-common python-software-properties -y
+apt-get install build-essential rsync telnet screen man wget -y
+#  apt-get install strace tcpdump -y
+apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
+apt-get install software-properties-common python-software-properties -y
 
-sudo apt-get install git -y
+apt-get install git -y
